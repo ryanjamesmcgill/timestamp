238ed60b6e3a46a2ebcb67670d9311074f3f0a47
Ryan James McGill <ryanjamesmcgill@gmail.com>
2016-05-25 17:41:03 -0500

    added process.env.port for heroku

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 routes(app);
 
-app.listen(3000, function () {
-   console.log('Node.js listening on port 3000...');
+app.listen((process.env.PORT || 3000), function () {
+   if(process.env.PORT){
+       console.log('Node.js listening on port ' + process.env.PORT + '...');
+   } else {
+       console.log('Node.js listening on port 3000...');
+   }
 });
